b51f26dcccd7b522357e668b055e56145a29223c
shalyn42k <tymirgundogar@gmail.com>
2025-10-06 13:29:21 +0200

    Rebuild project with Django and PostgreSQL

--- services/language_services.py
+++
@@ -1,7 +0,0 @@
-from flask import request, session
-
-# смена языка(думаю надо переделать)
-def get_language():
-    lang = request.args.get('lang', session.get('lang', 'pl'))
-    session['lang'] = lang
-    return lang
