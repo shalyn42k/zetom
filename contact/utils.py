3e16900b4e305aa4a54344847050dbcc5d50cc50
shalyn42k <tymirgundogar@gmail.com>
2025-12-05 15:50:02 +0000

    Update contact index template for i18n selector

--- contact/utils.py
+++ contact/utils.py
@@ -15,17 +15,11 @@ import hashlib
 
 from cryptography.fernet import Fernet, InvalidToken
 from django.conf import settings
+from django.utils.translation import get_language as django_get_language
 
 
 def get_language(request) -> str:
-    lang = request.GET.get('lang')
-    if lang:
-        request.session['lang'] = lang
-        return lang
-    session_lang = request.session.get('lang')
-    if session_lang:
-        return session_lang
-    return settings.DEFAULT_LANGUAGE
+    return getattr(request, "LANGUAGE_CODE", None) or django_get_language() or settings.LANGUAGE_CODE
 
 
 def get_client_ip(request) -> str:
